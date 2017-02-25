7b01ff63c59c8b327f25a2d11d4752057f6631b4
Jong Wook Kim <jongwook@nyu.edu>
2017-02-25 18:59:00 -0500

    add "hadoop.security" prefix to the defaultKeys

--- src/main/scala/com/kakao/cuesheet/HadoopUtils.scala
+++ src/main/scala/com/kakao/cuesheet/HadoopUtils.scala
@@ -11,21 +11,22 @@ import scala.collection.JavaConversions._
 object HadoopUtils {
 
   /** The Hadoop configuration keys for [[getHadoopXML]] */
-  val defaultKeys = Seq(
+  val defaultKeys: Seq[String] = Seq(
     CommonConfigurationKeysPublic.FS_DEFAULT_NAME_KEY,
     ZKFailoverController.ZK_QUORUM_KEY,
     DFSConfigKeys.DFS_NAMESERVICES,
     DFSConfigKeys.DFS_HA_NAMENODES_KEY_PREFIX,
     DFSConfigKeys.DFS_CLIENT_FAILOVER_PROXY_PROVIDER_KEY_PREFIX,
     DFSConfigKeys.DFS_HA_AUTO_FAILOVER_ENABLED_KEY,
-    DFSConfigKeys.DFS_NAMENODE_RPC_ADDRESS_KEY
+    DFSConfigKeys.DFS_NAMENODE_RPC_ADDRESS_KEY,
+    "hadoop.security"
   )
 
   /** Minimal XML to be able to connect to HDFS */
   def getHadoopXML(conf: Configuration): String = {
     YarnSparkHadoopUtil.escapeForShell(<configuration>{
       conf.iterator().collect {
-        case entry if defaultKeys.exists(entry.getKey.startsWith) || entry.getKey.startsWith("hadoop.security") =>
+        case entry if defaultKeys.exists(entry.getKey.startsWith) =>
           <property><name>{entry.getKey}</name><value>{entry.getValue}</value></property>
       }
     }</configuration>.toString())
