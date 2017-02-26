38563d348e1270cf17cf979616fe79ea8d773e0e
Jong Wook Kim <jongwook@nyu.edu>
2017-02-26 18:36:34 -0500

    Hive example more readable

--- src/test/scala/com/kakao/cuesheet/examples/SimpleHiveExample.scala
+++ src/test/scala/com/kakao/cuesheet/examples/SimpleHiveExample.scala
@@ -3,5 +3,5 @@ package com.kakao.cuesheet.examples
 import com.kakao.cuesheet.CueSheet
 
 object SimpleHiveExample extends CueSheet {{
-  hiveContext.sql("show databases").show(false)
+  hiveContext.sql("show databases").show(truncate = false)
 }}
